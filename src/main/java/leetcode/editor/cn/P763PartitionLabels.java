faf28bc25affb237ae0fc373f014146deaa7c157
dedeguo <chende2018@gmail.com>
2021-04-11 23:37:29 +0800

    #763 todo solve # 337.  #739 方法不是最优 效率高、代码简洁

---
+++ src/main/java/leetcode/editor/cn/P763PartitionLabels.java
@@ -0,0 +1,48 @@
+
+package leetcode.editor.cn;
+//字符串 S 由小写字母组成。我们要把这个字符串划分为尽可能多的片段，同一字母最多出现在一个片段中。返回一个表示每个字符串片段的长度的列表。 
+//
+// 
+//
+// 示例： 
+//
+// 
+//输入：S = "ababcbacadefegdehijhklij"
+//输出：[9,7,8]
+//解释：
+//划分结果为 "ababcbaca", "defegde", "hijhklij"。
+//每个字母最多出现在一个片段中。
+//像 "ababcbacadefegde", "hijhklij" 的划分是错误的，因为划分的片段数较少。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// S的长度在[1, 500]之间。 
+// S只包含小写字母 'a' 到 'z' 。 
+// 
+// Related Topics 贪心算法 双指针 
+// 👍 481 👎 0
+
+
+//date: 2021-04-11 23:18:01
+
+import java.util.List;
+
+public class P763PartitionLabels{
+    public static void main(String[] args) {
+        Solution solution = new P763PartitionLabels().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}

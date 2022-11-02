fdedc1d52938eb7297c573b28d549b67f6558b0d
dedeguo <chende2018@gmail.com>
2022-11-02 10:30:20 +0800

    slove #206
    头插法
    todo 递归法

---
+++ src/main/java/leetcode/editor/cn/P206ReverseLinkedList.java
@@ -0,0 +1,101 @@
+
+package leetcode.editor.cn;
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+// 
+// 
+// 
+// 
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+//
+// Related Topics 递归 链表 👍 2832 👎 0
+
+
+//date: 2022-11-02 10:17:09
+
+import leetcode.editor.definedata.ListNode;
+
+public class P206ReverseLinkedList{
+    public static void main(String[] args) {
+        Solution solution = new P206ReverseLinkedList().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //头插法
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode newHead = new ListNode();
+        ListNode p=head;
+        while (p!=null){
+            ListNode temp=p;
+            p=p.next;
+            temp.next=newHead.next;
+            newHead.next=temp;
+        }
+        return newHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+    //递归
+class SolutionV {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
+//
+//    作者：LeetCode-Solution
+//    链接：https://leetcode.cn/problems/reverse-linked-list/solution/fan-zhuan-lian-biao-by-leetcode-solution-d1k2/
+//    来源：力扣（LeetCode）
+//    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
